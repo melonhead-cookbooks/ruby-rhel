2029db2bf2bf1b2a1cc3bcf87b93c85506cafc98
Danny Knapp <dknapp@salesforce.com>
2014-06-20 16:28:05 -0700

    First commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'ruby-rhel'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures ruby-rhel'
+long_description 'Installs/Configures ruby-rhel'
+version          '0.1.0'
+
