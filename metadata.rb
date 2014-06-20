abc8e4d46bd60ecd80a0aeb6208526fe3de20ee9
Danny Knapp <dknapp@salesforce.com>
2014-06-20 16:39:15 -0700

    Tweaking recipe for installation

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,7 @@
 name             'ruby-rhel'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'Danny Knapp'
+maintainer_email 'dknapp@salesforce.com'
 license          'All rights reserved'
 description      'Installs/Configures ruby-rhel'
 long_description 'Installs/Configures ruby-rhel'
 version          '0.1.0'
-
